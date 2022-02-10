e9191da8c8c0b89624d767d017e78ee4f076db8b
Robert Albenda <ralbenda@gmail.com>
2022-02-10 09:54:37 -0500

    added routes

--- server.js
+++ server.js
@@ -1,63 +1,11 @@
-const express = require('express')
+const express = require('express');
 const database = require ('./database./database')
 const app = express();
 
-app.get('/', (req,res) => res.redirect('/family'))
 
-app.get('/family', async (req,res,next) => {
-    try {
-        const sql = `SELECT * FROM family_members`;
-        const response = await database.query(sql);
-        const family = response.rows;
-        const html = family.map( person => {
-        return `
-        
-        <div>
-            <a href = '/family/${person.id}'> ${ person.name } </a>
-        </div>
-        `;
-        }).join('');
-        res.send(`
-        <h1> Robby's Family Page</h1>
-        ${html}`)
-    }
+app.get('/', (req,res) => res.redirect('/family'));
 
-    catch (ex){
-        next(ex)
-    }
-    })
-
-    app.get('/family/:id', async (req,res,next) => {
-        try {
-            const sql = `SELECT * FROM family_about JOIN family_members ON family_about.family_id=family_members.id WHERE family_id=$1`;
-            const response = await database.query(sql, [req.params.id]);
-            const family = response.rows[0];
-           
-            res.send(`
-            <html>
-                <body>
-                    <title> Family About Page </title>
-                    <H1>Robby's Family Page </h1>
-                    <div>
-                        
-                            <ul> ${family.name}'s favorite hobby: ${family.hobby1}</ul>
-                            <ul> ${family.name}'s favorite team: ${family.favorite_team}</ul>
-                       
-                    </div>
-                </body>
-            </html>
-            
-            
-            `)
-        }
-    
-        catch (ex){
-            next(ex)
-        }
-        })
-
- 
-
+app.use('/family', require('./product-route'))
   
 
 const startUp = async() => {
